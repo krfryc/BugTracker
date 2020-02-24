d2f2e625abc1db2942d24258709762d44d7898ae
Krzysztof Fryc <kr.fryc@gmail.com>
2020-02-24 12:27:45 +0100

    fixing thymeleaf to work on Heroku

---
+++ src/main/java/pl/kfryc/bugtracker/config/ThymeleafConfig.java
@@ -0,0 +1,10 @@
+package pl.kfryc.bugtracker.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class ThymeleafConfig {
+
+}
