ecfeb7a40b3a3d87acc194d9d2ad8f5c16dc0f9a
Krzysztof Fryc <kr.fryc@gmail.com>
2020-02-24 01:17:40 +0100

    Commit

--- src/main/java/pl/kfryc/bugtracker/BugTrackerApplication.java
+++ src/main/java/pl/kfryc/bugtracker/BugTrackerApplication.java
@@ -3,9 +3,11 @@ package pl.kfryc.bugtracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import pl.kfryc.bugtracker.config.StorageProperties;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 @EnableConfigurationProperties(StorageProperties.class)
 public class BugTrackerApplication {
 
